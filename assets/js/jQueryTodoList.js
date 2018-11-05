39efea3ebf08827cc0966aae77e1dc71443d50ae
Nikolas9896 <nhlynka@gmail.com>
2018-11-05 11:58:34 +0200

    Set toggle method for class complieted. And fix issue in complited class

--- assets/js/jQueryTodoList.js
+++ assets/js/jQueryTodoList.js
@@ -1,6 +1,10 @@
 //Check off Specific To-dos By Clicking
 $('li').click( function(){
-	//if li is gray
+$(this).toggleClass("completed");
+
+
+//************ Test version class .Complited **************
+/*	//if li is gray
 	if($(this).css("color")==="rgb(128, 128, 128)"){
 		//turn it black
 			$(this).css({
@@ -14,4 +18,5 @@ $('li').click( function(){
 				color: "gray",
 				textDecoration: "line-through"});
 		}
+*/
 });
