8b94291e0743fcfed07d7386b733e8b60baa216a
Nikolas9896 <nhlynka@gmail.com>
2018-11-05 11:53:37 +0200

    Test completed logic

--- assets/js/jQueryTodoList.js
+++ assets/js/jQueryTodoList.js
@@ -1,5 +1,17 @@
 //Check off Specific To-dos By Clicking
 $('li').click( function(){
-	$(this).css("color", "gray");
-	$(this).css("text-decoration", "line-through");
+	//if li is gray
+	if($(this).css("color")==="rgb(128, 128, 128)"){
+		//turn it black
+			$(this).css({
+			color: "black",
+			textDecoration: "none"});
+	}
+	else {
+		//turn it gray
+	
+			$(this).css({
+				color: "gray",
+				textDecoration: "line-through"});
+		}
 });
